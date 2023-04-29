a66c1affd0cef1f1944d7fe454c9d7f6fa131d9a
Aden Herold <aden.herold1@gmail.com>
2023-04-30 09:37:29 +1000

    feat: added type declarations to types.d.ts

---
+++ src/types.d.ts
@@ -0,0 +1,31 @@
+declare namespace Dictionary {
+  declare type PartOfSpeech = "verb" | "noun" | "exclamation";
+
+  declare interface Phonetic {
+    text: string;
+    audio: string;
+    sourceUrl?: string;
+  }
+
+  declare interface Definition {
+    definition: string;
+    synonyms: string[];
+    antonyms: string[];
+    example?: string;
+  }
+
+  declare interface Meaning {
+    partOfSpeech: PartOfSpeech;
+    definitions: Definition[];
+    synonyms: string[];
+    antonyms: string[];
+  }
+
+  declare interface Entry {
+    word: string;
+    phonetic: "string";
+    phonetics: Phonetics[];
+    meanings: Meaning[];
+    sourceUrls: string[];
+  }
+}
