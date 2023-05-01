0c62486ca4eb843cfbce46d6e67c432cdedd93ee
Aden Herold <aden.herold1@gmail.com>
2023-05-01 10:16:19 +1000

    feat: added page structure to DictionaryWord component

--- src/components/DictionaryWord/DictionaryWord.tsx
+++ src/components/DictionaryWord/DictionaryWord.tsx
@@ -1,4 +1,6 @@
 import WordHeader from "@app/components/WordHeader";
+import WordSource from "@app/components/WordSource";
+import WordMeanings from "@app/components/WordMeanings";
 
 export interface DictionaryWordProps {
   word: Dictionary.Entry;
@@ -8,6 +10,8 @@ export default function DictionaryWord({ word }: DictionaryWordProps) {
   return (
     <>
       <WordHeader word={word} />
+      <WordMeanings meanings={word.meanings} />
+      <WordSource sourceUrls={word.sourceUrls} />
     </>
   );
 }
