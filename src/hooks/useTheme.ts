268ba6527053a0f07c8d0cb952727e6b3563e7cd
Aden Herold <aden.herold1@gmail.com>
2023-05-04 12:14:04 +1000

    docs: documented useTheme and theme utils functions

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -10,6 +10,12 @@ export default function useTheme() {
   const isDarkMode = theme === "dark";
 
   useEffect(() => {
+    /**
+     * An event listener is added to `(prefers-color-scheme: dark)` media
+     * query in the event that the user updates the color scheme of their
+     * operating system.
+     */
+
     matchDarkColorScheme.addEventListener("change", onOSThemeUpdate);
 
     return () => {
@@ -18,6 +24,10 @@ export default function useTheme() {
   }, []);
 
   useEffect(() => {
+    /* 
+      When the theme is updated, sets the data-theme attribute in order
+      to apply stylesheet theme variables.
+    */
     document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
 
--- src/lib/utils/theme.ts
+++ src/lib/utils/theme.ts
@@ -1,24 +1,39 @@
-const defaultTheme: Theme = "light";
-
 export const matchDarkColorScheme = window.matchMedia(
   "(prefers-color-scheme: dark)"
 );
 
+/**
+ * Retrieves either a client's preferred color theme
+ * from localStorage, or the color scheme preferred by the
+ * client's operating system.
+ */
 export const getThemeOrDefault = (): Theme => {
   const themePreference = getThemePreference();
   if (themePreference !== null) return themePreference;
-  return getOSPreferredColorScheme() || defaultTheme;
+  return getOSPreferredColorScheme();
 };
 
-export const getOSPreferredColorScheme = (): Theme | null => {
-  if (!window.matchMedia) return null;
-
+/**
+ * Returns dark color theme if `prefers-color-scheme: dark`
+ * media query is evident on client. This is typically determined
+ * by the operating system or user agent.
+ *
+ * Read more:
+ * https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme
+ */
+export const getOSPreferredColorScheme = (): Theme => {
   return matchDarkColorScheme.matches ? "dark" : "light";
 };
 
+/**
+ * Caches the client's preferred color theme in localStorage.
+ */
 export const setThemePreference = (preference: Theme) => {
   localStorage.setItem("themePreference", preference);
 };
 
+/**
+ * Retrieves the client's preferred color theme from localStorage.
+ */
 export const getThemePreference = (): Theme | null =>
   localStorage.getItem("themePreference") as Theme;
