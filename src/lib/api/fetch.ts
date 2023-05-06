b31dbf8eddc8f9f7493fcd706189d091d6537461
Aden Herold <aden.herold1@gmail.com>
2023-05-07 09:20:59 +1000

    refactor: moved src/api modules into src/lib/api directory

--- src/lib/api/fetch.ts
+++ src/lib/api/fetch.ts
@@ -2,6 +2,6 @@ type GetConfig = Omit<RequestInit, "method">;
 
 export async function get<T>(uri: string, config?: GetConfig): Promise<T> {
   const res = await fetch(uri, { ...config, method: "GET" });
-  if (!res.ok) throw Error();
+  if (!res.ok) throw res;
   return res.json();
 }
