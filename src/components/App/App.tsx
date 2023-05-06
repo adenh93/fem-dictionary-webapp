bcbd922f2f54fd5bc00aa9f2b9f6854045bfb756
Aden Herold <aden.herold1@gmail.com>
2023-05-07 09:23:30 +1000

    feat: wired up NotFound component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,10 +4,29 @@ import DictionaryWord from "@app/components/DictionaryWord";
 import useDictionarySearch from "@app/hooks/useDictionarySearch";
 import ThemeToggle from "@app/components/ThemeToggle";
 import FontSelector from "@app/components/FontSelector";
+import NotFound from "@app/components/NotFound";
 import styles from "./App.module.css";
 
+const initialSearch = "keyboard";
+
 export default function App() {
-  const { words, onUpdateInput } = useDictionarySearch();
+  const { words, notFound, onUpdateInput } = useDictionarySearch(initialSearch);
+
+  const getContent = () => {
+    if (notFound)
+      return (
+        <div className={styles.notFound}>
+          <NotFound />
+        </div>
+      );
+    else if (words)
+      return (
+        <div className={styles.dictionaryWord}>
+          <DictionaryWord word={words[0]} />
+        </div>
+      );
+    else return <></>;
+  };
 
   return (
     <main>
@@ -18,13 +37,12 @@ export default function App() {
           <ThemeToggle />
         </div>
       </header>
-      <div className={styles.search}>
-        <SearchInput
-          placeholder="Search for any word..."
-          onChange={onUpdateInput}
-        />
-      </div>
-      {words ? <DictionaryWord word={words[0]} /> : <></>}
+      <SearchInput
+        defaultValue={initialSearch}
+        placeholder="Search for any word..."
+        onChange={onUpdateInput}
+      />
+      {getContent()}
     </main>
   );
 }
